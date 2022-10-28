a1befcfd9cac0e5bceb5af46868a77ec7532aa3f
Eustache97 <eubomber97@gmail.com>
2022-10-28 07:39:53 +0200

    milestone1-2-3

--- js/script.js
+++ js/script.js
@@ -15,23 +15,32 @@ createApp({
     methods: {
 
         addNewTask: function(){
+            //se la lunghezza del testo inserit è inferiore a 5
             if(this.newTask.text.length < 5){
+                //flag = true
                 this.hasError = true;
             }else{
+                //altrimenti lo pushiamo nell'array
                 this.tasks.push({...this.newTask});
+                //resettiamo l'input e il flag a lo stato iniziale
                 this.newTask.text = "";
                 this.hasError = false;
             }        
         },
         taskDoneNotDone: function(taskIndex){
+            //se done è false
             if(this.tasks[taskIndex].done === false){
+                //diventa true
                 this.tasks[taskIndex].done = true;
             }else{
+                // altrimenti diventa false
                 this.tasks[taskIndex].done = false;
             }
             console.log(this.tasks[taskIndex].done);
+        },
+        deleteTask: function(taskIndex){
+            this.tasks.splice(taskIndex, 1);
         }
-        // deleteTask: function(){
             
          }
 }).mount("#app");
